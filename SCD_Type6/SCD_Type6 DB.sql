cde547ed35cdefb70aafbefbc24b5484044955c3
nerminhassan2 <117374886+nerminhassan2@users.noreply.github.com>
2023-06-22 10:39:48 +0300

    Update SCD_Type6 DB.sql

--- SCD_Type6/SCD_Type6 DB.sql
+++ SCD_Type6/SCD_Type6 DB.sql
@@ -27,23 +27,23 @@ update Employees set City = 'Cairo', Update_Date = getDate() where ID = 1001
 update Employees set City = 'Alex', Email = 'alaa123@mail.com', Update_Date = getDate() where ID = 1002
 
 
-select * from [dbo].[Employees]
-select * from [dbo].[Employees_SCD] 
+select * from Employees
+select * from Employees_SCD
 
 /*
-Example of the result set(excuting query 'select * from Employees'):
+Example of the result set(executing query 'select * from Employees'):
 
  ID     Name    City      Email                Update_Date
 1001	Ahmed	Cairo	ahmed@mail.com	   2023-06-22 09:28:27.130
 1002	Alaa	Alex	alaa123@mail.com   2023-06-22 09:28:27.130
 1003	Samy	Cairo	samy@mail.com	   2023-05-18 09:04:23.473
 
-Example of the result set(excuting query 'select * from Employees_SCD'):
+Example of the result set(executing query 'select * from Employees_SCD'):
 
-Emp_key	  ID	 Name	Current_City	Previous_City	Current_Email	     Previous_Email	         Start_Date	                   End_Data	         Active_Flag
-1	     1001	Ahmed	   Cairo	       Giza	        ahmed@mail.com	     ahmed@mail.com	    2023-06-19 09:27:59.350	    2023-06-22 09:28:41.847	      0
-2	     1002	Alaa	   Alex	           Giza	        alaa123@mail.com	 alaa@mail.com	    2023-06-19 09:27:59.350	    2023-06-22 09:28:41.850	      0
-3	     1003	Samy	   Cairo	       Cairo	    samy@mail.com	     samy@mail.com	    2023-06-19 09:27:59.350	    NULL	                      1
-4	     1001	Ahmed	   Cairo	       Cairo	    ahmed@mail.com	     ahmed@mail.com	    2023-06-22 09:28:41.903	    NULL	                      1
-5	     1002	Alaa	   Alex	           Alex	        alaa123@mail.com	 alaa123@mail.com	2023-06-22 09:28:41.903	    NULL	                      1
+Emp_key	      ID	Name	Current_City	Previous_City	Current_Email	     Previous_Email	         Start_Date	                   End_Data	         Active_Flag
+1	     1001	Ahmed	   Cairo	   Giza	        ahmed@mail.com	     ahmed@mail.com	    2023-06-19 09:27:59.350	    2023-06-22 09:28:41.847	      0
+2	     1002	Alaa	   Alex	           Giza	        alaa123@mail.com     alaa@mail.com	    2023-06-19 09:27:59.350	    2023-06-22 09:28:41.850	      0
+3	     1003	Samy	   Cairo	   Cairo	samy@mail.com	     samy@mail.com	    2023-06-19 09:27:59.350	    NULL	                      1
+4	     1001	Ahmed	   Cairo	   Cairo	ahmed@mail.com	     ahmed@mail.com	    2023-06-22 09:28:41.903	    NULL	                      1
+5	     1002	Alaa	   Alex	           Alex	        alaa123@mail.com     alaa123@mail.com	    2023-06-22 09:28:41.903	    NULL	                      1
 */
