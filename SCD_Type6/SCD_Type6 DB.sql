44aa1afb283fdcd236c151a74c5756ec0b307899
nermin <nerminhassan2820@gmail.com>
2023-06-22 10:35:47 +0300

    intial commit

---
+++ SCD_Type6/SCD_Type6 DB.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Employees_SCD(
+	Emp_key int identity(1,1) primary key,
+	ID int not null,
+	Name varchar(50),
+	Current_City varchar(50),
+	Previous_City varchar(50),
+	Current_Email varchar(50),
+	Previous_Email varchar(50),
+	Start_Date Datetime,
+	End_Data Datetime,
+	Active_Flag bit
+);
+
+CREATE TABLE Employees(
+	ID int identity(1001,1) primary key,
+	Name varchar(50),
+	City varchar(50),
+	Email varchar(50),
+	Update_Date Datetime
+);
+
+insert into Employees(Name, City, Email, Update_Date) values('Ahmed','Giza','ahmed@mail.com','2023-06-22 09:28:27.130');
+insert into Employees(Name, City, Email, Update_Date) values('Alaa','Giza','alaa@mail.com','2023-06-22 09:28:27.130');
+insert into Employees(Name, City, Email, Update_Date) values('Samy','Cairo','samy@mail.com','2023-06-22 09:28:27.130');
+
+update Employees set City = 'Cairo', Update_Date = getDate() where ID = 1001
+update Employees set City = 'Alex', Email = 'alaa123@mail.com', Update_Date = getDate() where ID = 1002
+
+
+select * from [dbo].[Employees]
+select * from [dbo].[Employees_SCD] 
+
+/*
+Example of the result set(excuting query 'select * from Employees'):
+
+ ID     Name    City      Email                Update_Date
+1001	Ahmed	Cairo	ahmed@mail.com	   2023-06-22 09:28:27.130
+1002	Alaa	Alex	alaa123@mail.com   2023-06-22 09:28:27.130
+1003	Samy	Cairo	samy@mail.com	   2023-05-18 09:04:23.473
+
+Example of the result set(excuting query 'select * from Employees_SCD'):
+
+Emp_key	  ID	 Name	Current_City	Previous_City	Current_Email	     Previous_Email	         Start_Date	                   End_Data	         Active_Flag
+1	     1001	Ahmed	   Cairo	       Giza	        ahmed@mail.com	     ahmed@mail.com	    2023-06-19 09:27:59.350	    2023-06-22 09:28:41.847	      0
+2	     1002	Alaa	   Alex	           Giza	        alaa123@mail.com	 alaa@mail.com	    2023-06-19 09:27:59.350	    2023-06-22 09:28:41.850	      0
+3	     1003	Samy	   Cairo	       Cairo	    samy@mail.com	     samy@mail.com	    2023-06-19 09:27:59.350	    NULL	                      1
+4	     1001	Ahmed	   Cairo	       Cairo	    ahmed@mail.com	     ahmed@mail.com	    2023-06-22 09:28:41.903	    NULL	                      1
+5	     1002	Alaa	   Alex	           Alex	        alaa123@mail.com	 alaa123@mail.com	2023-06-22 09:28:41.903	    NULL	                      1
+*/
---
+++ Versioning/Versioning problem DB.sql
@@ -0,0 +1,55 @@
+CREATE TABLE Employees_versions(
+	Emp_key int identity(1,1) primary key,
+	ID int not null,
+	Name varchar(50),
+	City varchar(50),
+	Email varchar(50),
+	Insert_Date Date,
+	Active_Flag bit,
+	Version_No int 
+);
+
+CREATE TABLE Employees_Q3(
+	ID int identity(1001,1) primary key,
+	Name varchar(50),
+	City varchar(50),
+	Email varchar(50),
+	Schedule_Date Date
+);
+
+insert into Employees_Q3(Name, City, Email, Schedule_Date) values('Ahmed','Giza','ahmed@mail.com','2023-05-18');
+insert into Employees_Q3(Name, City, Email, Schedule_Date) values('Alaa','Giza','alaa@mail.com','2023-05-18');
+insert into Employees_Q3(Name, City, Email, Schedule_Date) values('Samy','Cairo','samy@mail.com','2023-05-18');
+
+select * from [dbo].[Employees_Q3]
+select * from [dbo].[Employees_versions] 
+
+
+/*
+Example of the result set(excuting query 'select * from Employees_Q3'):
+
+ID	    Name	City	   Email	    Schedule_Date
+1001	Ahmed	Giza	ahmed@mail.com	2023-05-18
+1002	Alaa	Giza	alaa@mail.com	2023-05-18
+1003	Samy	Cairo	samy@mail.com	2023-05-18
+
+Example of the result set(excuting query 'select * from Employees_versions'):
+
+Emp_key  ID     Name    City       Email         Insert_Date   Active_Flag    Version_No
+1	    1001	Ahmed	Giza	ahmed@mail.com	 2023-05-18	      0	             1
+2	    1002	Alaa	Giza	alaa@mail.com	 2023-05-18	      0	             1
+3	    1003	Samy	Cairo	samy@mail.com	 2023-05-18	      0	             1
+4	    1001	Ahmed	Giza	ahmed@mail.com	 2023-05-18	      0	             2
+5	    1002	Alaa	Giza	alaa@mail.com	 2023-05-18	      0	             2
+6	    1003	Samy	Cairo	samy@mail.com	 2023-05-18	      0	             2
+6	    1003	Samy	Cairo	samy@mail.com	 2023-05-18	      0	             2
+7	    1001	Ahmed	Giza	ahmed@mail.com	 2023-05-18	      0	             3
+8	    1002	Alaa	Giza	alaa@mail.com	 2023-05-18	      0	             3
+9	    1003	Samy	Cairo	samy@mail.com	 2023-05-18	      0	             3
+10	    1001	Ahmed	Giza	ahmed@mail.com	 2023-06-22	      0	             1
+11	    1002	Alaa	Giza	alaa@mail.com	 2023-06-22	      0	             1
+12	    1003	Samy	Cairo	samy@mail.com	 2023-06-22	      0	             1
+13	    1001	Ahmed	Giza	ahmed@mail.com	 2023-06-22	      1	             2
+14	    1002	Alaa	Giza	alaa@mail.com	 2023-06-22	      1	             2
+15	    1003	Samy	Cairo	samy@mail.com	 2023-06-22	      1	             2
+*/
